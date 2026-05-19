35987f16f4d25d2173b90a66aaa91d001df9be29
Luiza Schneider <luizass.work@gmail.com>
2026-05-19 16:32:00 -0300

    class vaga

---
+++ skillmatch.js
@@ -0,0 +1,45 @@
+class Vaga {
+  constructor(
+    empresa,
+    cargo,
+    compatibilidade,
+    habilidadesEncontradas,
+    habilidadesFaltantes,
+    classificacao,
+  ) {
+    this.empresa = empresa;
+    this.cargo = cargo;
+    this.compatibilidade = compatibilidade;
+    this.habilidadesEncontradas = habilidadesEncontradas;
+    this.habilidadesFaltantes = habilidadesFaltantes;
+    this.classificacao = classificacao;
+  }
+}
+
+const luiza = {
+  nome: "Luiza",
+  area: "Front-End",
+  habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "Kanban"],
+  tempoDeExperienciaMeses: 3,
+};
+
+const vagasDeEmprego = [
+  {
+    id: 1,
+    empresa: "TechStart",
+    cargo: "Desenvolvedor Front-End Júnior",
+    requisitos: ["JavaScript", "GitHub", "Lógica de Programação"],
+  },
+  {
+    id: 2,
+    empresa: "CodeLab",
+    cargo: "Estágio Front-End",
+    requisitos: ["JavaScript", "Kanban", "GitHub"],
+  },
+  {
+    id: 3,
+    empresa: "WebSolutions",
+    cargo: "Programador JavaScript Júnior",
+    requisitos: ["JavaScript", "Arrays", "Objetos", "Funções"],
+  },
+];
