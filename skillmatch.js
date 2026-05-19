f6b92d011b6286dc31e5c716d51414506903d0fd
Luiza Schneider <luizass.work@gmail.com>
2026-05-19 19:49:29 -0300

    feat:final

--- skillmatch.js
+++ skillmatch.js
@@ -16,29 +16,6 @@ class Vaga {
   }
 }
 
-class ResultadoVaga extends Vaga {
-  constructor(
-    empresa,
-    cargo,
-    compatibilidade,
-    habilidadesEncontradas,
-    habilidadesFaltantes,
-    classificacao,
-    nivel,
-  ) {
-    super(
-      empresa,
-      cargo,
-      compatibilidade,
-      habilidadesEncontradas,
-      habilidadesFaltantes,
-      classificacao,
-    );
-
-    this.nivel = nivel;
-  }
-}
-
 const luiza = {
   nome: "Luiza",
   area: "Front-End",
@@ -105,3 +82,38 @@ function compararCandidatoVagas(candidato, vagas) {
 
   return resultados;
 }
+function faltaEstudar(candidato, vagas) {
+  const contadorMaterias = {};
+
+  for (const vaga of vagas) {
+    for (const requisito of vaga.requisitos) {
+      if (!candidato.habilidades.includes(requisito)) {
+        contadorMaterias[requisito] = (contadorMaterias[requisito] || 0) + 1;
+      }
+    }
+  }
+
+  return Object.entries(contadorMaterias)
+    .sort((a, b) => b[1] - a[1])
+    .map(([materia, qtd]) => `${materia} (${qtd} vaga(s))`);
+}
+
+function gerarRelatorio(candidato, vagas) {
+  const resultados = compararCandidatoVagas(candidato, vagas);
+  const recomendacoes = faltaEstudar(candidato, vagas);
+
+  console.log("📊 RESULTADO DAS VAGAS:\n");
+  console.log(resultados);
+
+  console.log("\n📚 RECOMENDAÇÃO DE ESTUDO:\n");
+
+  if (recomendacoes.length === 0) {
+    console.log("Você já atende todas as exigências das vagas!");
+  } else {
+    console.log("Priorize estudar:");
+    recomendacoes.forEach((item) => console.log("- " + item));
+    console.log("pois esses conteúdos aparecem nas vagas analisadas.");
+  }
+}
+
+gerarRelatorio(luiza, vagasDeEmprego);
