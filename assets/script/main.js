474d3e3809ff6655615ad19b412dd01b3c410fd4
Luiza Schneider <luizass.work@gmail.com>
2026-07-14 19:50:04 -0300

    implementação de ui
    foi implementado o ui.js e um pouco do motor.js com o main.js

---
+++ assets/script/main.js
@@ -0,0 +1,5 @@
+import { iniciarBotoesHabilidades, iniciarFormulario } from "./ui.js";
+import { Vaga } from "./motor.js";
+
+iniciarBotoesHabilidades();
+iniciarFormulario();
---
+++ assets/script/motor.js
@@ -0,0 +1,45 @@
+export class Vaga {
+  constructor(id, empresa, cargo, requisitos, salario, modalidade) {
+    this.id = id;
+    this.empresa = empresa;
+    this.cargo = cargo;
+    this.requisitos = requisitos;
+    this.salario = salario;
+    this.modalidade = modalidade;
+  }
+
+  calcularCompatibilidade(habilidadesCandidato) {
+    const encontradas = this.requisitos.filter((req) =>
+      habilidadesCandidato.includes(req),
+    );
+    const faltantes = this.requisitos.filter(
+      (req) => !habilidadesCandidato.includes(req),
+    );
+    const percentual = (encontradas.length / this.requisitos.length) * 100;
+
+    return { encontradas, faltantes, percentual };
+  }
+}
+
+export class VagaFrontEnd extends Vaga {
+  constructor(id, empresa, cargo, requisitos, salario, modalidade, stack) {
+    super(id, empresa, cargo, requisitos, salario, modalidade);
+    this.stack = stack;
+    this.habilidadesPeso = ["JavaScript", "GitHub"];
+  }
+
+  calcularCompatibilidade(habilidadesCandidato) {
+    const resultadoBase = super.calcularCompatibilidade(habilidadesCandidato);
+
+    const pesoExtra = resultadoBase.encontradas.filter((hab) =>
+      this.habilidadesPeso.includes(hab),
+    ).length;
+
+    const percentualComPeso = Math.min(
+      100,
+      resultadoBase.percentual + pesoExtra * 5,
+    );
+
+    return { ...resultadoBase, percentual: percentualComPeso };
+  }
+}
